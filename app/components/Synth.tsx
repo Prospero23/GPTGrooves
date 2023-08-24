5e88dea89b369f9793f2b121cfcb85b5d779bf11
Ben Eidson <b.e.eidson@gmail.com>
2023-08-24 15:37:04 -0700

    Type guard idea in progress

--- app/components/Synth.tsx
+++ app/components/Synth.tsx
@@ -1,25 +1,51 @@
-'use client'
+"use client";
 
 import { createDevice } from "@rnbo/js";
 import { useEffect, useState } from "react";
+import Instrument from "../../synth_resources/drums.export.json"
 
 export default async function Synth() {
-
-    const [waContext, setWaContext] = useState<undefined | AudioContext>(undefined)
-
-    useEffect(() => {
-        //@ts-ignore
-        setWaContext(window.AudioContext || window.webkitAudioContext)
-        // On dismount, clear state
-        return () => { setWaContext(undefined) }
-    }, [])
+  const [waContext, setWaContext] = useState<undefined | AudioContext>(
+    undefined
+  );
 
 
+  const setup = async () => {
+    // let rawPatcher = await fetch("patcher.export.json");
+    // let patcher = await rawPatcher.json();
 
-    //const device = await createDevice({context, patcher})
+    const baseInstrument = Instrument
+    // We don't have options in the exported value. IPatcher.desc needs it.
+    // baseInstrument.desc["options"] = {
+    //     classname: "fuck",
+    //     minifyOutput: false // TODO is this cool should we try this?
+    // }
+    baseInstrument.desc["options"] = undefined
+    const fixedInstrument: IPatcher = baseInstrument
+    let device = await createDevice({ waContext, patcher: fixedInstrument });
+
+    // This connects the device to audio output, but you may still need to call context.resume()
+    // from a user-initiated function.
+    device.node.connect(waContext.destination);
+  };
+
+  useEffect(() => {
+    //@ts-ignore
+    setWaContext(window.AudioContext || window.webkitAudioContext);
+
+    setup();
+    // On dismount, clear state
+    return () => {
+      setWaContext(undefined);
+    };
+  }, []);
+
+
+
+  //const device = await createDevice({context, patcher})
   return (
     <div className="text-white">
-    <h3>TESTSTESTSTS</h3>
+      <h3>TESTSTESTSTS</h3>
     </div>
   );
 }
