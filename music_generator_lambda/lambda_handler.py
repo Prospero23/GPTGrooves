cc5a6d0e1ae3180554f60e4c1920f2b504082975
Kevin Katz <kevcmk@gmail.com>
2023-08-31 13:35:30 -0700

    Invoke generate in lambda. Fix types.

--- music_generator_lambda/lambda_handler.py
+++ music_generator_lambda/lambda_handler.py
@@ -2,11 +2,13 @@ import json
 import os
 import sys
 
-
 from boto3.session import Session
 from botocore.exceptions import ClientError
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from mypy_boto3_secretsmanager.client import SecretsManagerClient
+
+from bson.raw_bson import RawBSONDocument
 
 # TODO Remove this?
 sys.path.append(os.path.join(os.path.dirname(__file__)))
@@ -15,7 +17,7 @@ sys.stderr.write("PATH")
 sys.stderr.write(",".join(sys.path))
 sys.stderr.write("\n")
 
-from music_generator.generator import generate_bar
+from music_generator.generator import Bar, generate_bar
 from music_generator.utilities.logs import get_logger  # noqa: E402
 from music_generator.types import Config
 
@@ -24,7 +26,9 @@ logger = get_logger(__name__)
 
 def get_secret(session: Session, secret_id: str, region_name: str) -> dict[str, str]:
     # Create a Secrets Manager client
-    client = session.client(service_name="secretsmanager", region_name=region_name)
+    client: SecretsManagerClient = session.client(
+        service_name="secretsmanager", region_name=region_name
+    )
 
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_id)
@@ -42,7 +46,11 @@ def get_secret(session: Session, secret_id: str, region_name: str) -> dict[str, 
 
 def ping_database(config: Config) -> None:
     # Create a new client and connect to the server
-    client = MongoClient(config.atlas_cluster_uri, server_api=ServerApi("1"))
+    client = MongoClient(
+        config.atlas_cluster_uri,
+        server_api=ServerApi("1"),
+        document_class=RawBSONDocument,
+    )
     # Send a ping to confirm a successful connection
     try:
         client.admin.command("ping")
@@ -52,7 +60,7 @@ def ping_database(config: Config) -> None:
         raise e
 
 
-def handler(event, context):
+def handler(event, context):  # noqa
     secret_id = os.environ["SECRETS_MANAGER_SECRET_ID"]
     region = os.environ["AWS_REGION"]
     print(f"Region is: {region}. Secrets stored at {secret_id}.")
@@ -60,14 +68,20 @@ def handler(event, context):
     session = Session()
 
     secrets = get_secret(session=session, secret_id=secret_id, region_name=region)
-    config = Config(
-        openai_api_key=secrets["openai_api_key"],
-        atlas_cluster_uri=secrets["atlas_cluster_uri"],
-    )
+    # config = Config(
+    #     openai_api_key=secrets["openai_api_key"],
+    #     atlas_cluster_uri=secrets["atlas_cluster_uri"],
+    # )
+    config = Config(**secrets)
 
+    # Sanity checks # TODO Remove
     ping_database(config=config)
-    bar = generate_bar(config=Config(**secrets))
-    print(bar)
+    assert Bar.example() == Bar.example()
+    assert Bar.from_structured_text(Bar.example().to_structured_text()) == Bar.example()
+    assert Bar.from_llm_format(Bar.example().to_llm_format()) == Bar.example()
+
+    bar = generate_bar(config=config)  # type: ignore
+    logger.info(f"Generated Bar:\n{bar}")
 
     return {
         "statusCode": 200,
