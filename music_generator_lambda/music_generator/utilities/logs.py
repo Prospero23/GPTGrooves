6e16afe7c6b32faf2f05c1737a9e5c440c1caa29
Kevin Katz <kevcmk@gmail.com>
2023-09-11 12:10:17 +0300

    Simple logger

--- music_generator_lambda/music_generator/utilities/logs.py
+++ music_generator_lambda/music_generator/utilities/logs.py
@@ -20,25 +20,6 @@ def get_logger(name: str, level: Optional[int] = None):
     )
     # Create a logger
     logger = logging.getLogger(name)
-
-    # Avoid duplicate log messages if the logger is already configured
-    if not logger.handlers:
-        # Set the log level of the logger to DEBUG
-        logger.setLevel(level)
-
-        # Create a handler
-        handler = logging.StreamHandler()
-
-        # Set the log level of the handler to DEBUG
-        handler.setLevel(level)
-
-        # Create a formatter and add it to the handler
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        handler.setFormatter(formatter)
-
-        # Add the handler to the logger
-        logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
 
     return logger
