faabadfa9612371ee31df0e607fc7e3c14db5d99
Kevin Katz <kevcmk@gmail.com>
2023-09-29 11:02:02 -0700

    Comment out uused value

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,24 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
 import { useAnimations, useGLTF } from "@react-three/drei";
-import { Device, MIDIEvent, MIDIData, TimeNow, MessageEvent } from "@rnbo/js";
+import {
+  type Device,
+  MIDIEvent,
+  type MIDIData,
+  TimeNow,
+  MessageEvent,
+} from "@rnbo/js";
 import setup from "@/public/sound";
-import { Vector3 } from "three";
-import { BarType } from "@/library/musicData";
+import { type Vector3 } from "three";
+import { type BarType } from "@/library/musicData";
 import noteToMidi from "@/library/noteToMidi";
 
 interface ButtonProps {
   position: Vector3;
-  isPlaying: Boolean | undefined;
-  setIsPlaying: React.Dispatch<React.SetStateAction<Boolean | undefined>>;
-  //give data
-  playingData: Array<BarType>;
+  isPlaying: boolean | undefined;
+  setIsPlaying: React.Dispatch<React.SetStateAction<boolean | undefined>>;
+  // give data
+  playingData: BarType[];
 }
 
 const drumInlets = {
@@ -30,30 +36,30 @@ export default function Button({
   const { scene, animations } = useGLTF("/assets/butttton.glb");
   const { actions, names } = useAnimations(animations, scene);
 
-  //audio devices and context
+  // audio devices and context
   const audioContext = useRef<AudioContext | undefined>(undefined);
   const drums = useRef<Device | undefined>(undefined);
   const bass = useRef<Device | undefined>(undefined);
   const pad = useRef<Device | undefined>(undefined);
 
-  //sequence stuff
-  const startTime = useRef<number | undefined>(undefined); //start time of sequence
-  const currentStep = useRef<number>(0); //what step of bar is currently being scheduled?
-  const currentBar = useRef<number>(0); //current Bar
-  const tempo = 140.0; //current tempo (bpm)
-  const lookahead = 25.0; //how frequent to call schedule function in ms
-  const scheduleAheadTime = 0.1; //how far ahead to schedule audio in sec
-  let nextNoteTime = useRef<number>(0.0); //when next note is due
-  let noteResolution = 0; //0 is 16th, 1 - 8th, 2 - quarter
-  const timerID = useRef<number | undefined>(undefined); //setInterval identifier
+  // sequence stuff
+  // const startTime = useRef<number | undefined>(undefined); // start time of sequence
+  const currentStep = useRef<number>(0); // what step of bar is currently being scheduled?
+  const currentBar = useRef<number>(0); // current Bar
+  const tempo = 140.0; // current tempo (bpm)
+  const lookahead = 25.0; // how frequent to call schedule function in ms
+  const scheduleAheadTime = 0.1; // how far ahead to schedule audio in sec
+  const nextNoteTime = useRef<number>(0.0); // when next note is due
+  const noteResolution = 0; // 0 is 16th, 1 - 8th, 2 - quarter
+  const timerID = useRef<number | undefined>(undefined); // setInterval identifier
 
-  const notesInQueue = []; //FOR FUTURE VISUALS (see playBUTTON link)
+  const notesInQueue = []; // FOR FUTURE VISUALS (see playBUTTON link)
 
-  //setup function. RUNS ONCE
+  // setup function. RUNS ONCE
   useEffect(() => {
     // This code runs after the component has been rendered
     async function init() {
-      const result = await setup(); //TODO: CHECK THE GAIN NODE
+      const result = await setup(); // TODO: CHECK THE GAIN NODE
       if (result) {
         drums.current = result.device;
         bass.current = result.deviceBass;
@@ -72,14 +78,14 @@ export default function Button({
   }, []);
 
   function nextNote() {
-    //advance time to next 16th note //ADD BARS?
-    let secondsPerBeat = 60.0 / tempo;
+    // advance time to next 16th note //ADD BARS?
+    const secondsPerBeat = 60.0 / tempo;
 
     nextNoteTime.current += 0.25 * secondsPerBeat;
 
     currentStep.current++;
     if (currentStep.current == 16) {
-      //wrap 16 to 0
+      // wrap 16 to 0
       currentStep.current = 0;
       currentBar.current++;
     }
@@ -87,24 +93,24 @@ export default function Button({
 
   function scheduleNote(beatNumber: number, time: number) {
     // push the note on the queue, even if we're not playing.
-    //notesInQueue.push( { note: beatNumber, time: time } ); ADD BACK LATER
+    // notesInQueue.push( { note: beatNumber, time: time } ); ADD BACK LATER
 
     if (noteResolution == 1 && beatNumber % 2)
-      //only runs when res is set to 1
+      // only runs when res is set to 1
       return; // we're not playing non-8th 16th notes
     if (noteResolution == 2 && beatNumber % 4)
-      //only runs when res is set to 2
+      // only runs when res is set to 2
       return; // we're not playing non-quarter 8th notes
 
     // logic for scheduling
     if (audioContext.current && drums.current) {
-      //drums logic
-      for (let drumType in playingData[currentBar.current].drums) {
-        //check for correct structure
+      // drums logic
+      for (const drumType in playingData[currentBar.current].drums) {
+        // check for correct structure
         if (playingData[currentBar.current].drums.hasOwnProperty(drumType)) {
-          let inlet = drumInlets[drumType as keyof typeof drumInlets];
+          const inlet = drumInlets[drumType as keyof typeof drumInlets];
           const drumEventTrigger = new MessageEvent(time, `in${inlet}`, [
-            //@ts-ignore
+            // @ts-expect-error
             playingData[currentBar.current].drums[drumType][
               currentStep.current
             ],
@@ -112,64 +118,61 @@ export default function Button({
           drums.current.scheduleEvent(drumEventTrigger);
         }
       }
-      //bass
+      // bass
       const bassNote = noteToMidi(
-        playingData[currentBar.current].bass.pattern[currentStep.current]
+        playingData[currentBar.current].bass.pattern[currentStep.current],
       );
 
       if (!isNaN(bassNote)) {
         const bassEventTrigger = new MessageEvent(TimeNow, `in0`, [bassNote]);
         bass.current?.scheduleEvent(bassEventTrigger);
       }
-      //PAD
+      // PAD
       if (pad.current) {
-        let padInstance = pad.current as Device;
-        playingData[currentBar.current].pad.chord_sequence[currentStep.current].notes.forEach(
-          (note) => {
-            let midiChannel = 0;
+        const padInstance = pad.current;
+        playingData[currentBar.current].pad.chord_sequence[
+          currentStep.current
+        ].notes.forEach((note) => {
+          const midiChannel = 0;
 
+          const midiNote = noteToMidi(note) + 12;
 
+          // Format a MIDI message paylaod, this constructs a MIDI on event
+          const noteOnMessage: MIDIData = [
+            144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
+            midiNote, // MIDI Note
+            100, // MIDI Velocity
+          ];
 
-            let midiNote = noteToMidi(note) + 12;
+          const noteOffMessage: MIDIData = [
+            128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
+            midiNote, // MIDI Note
+            0, // MIDI Velocity
+          ];
 
-            // Format a MIDI message paylaod, this constructs a MIDI on event
-           let noteOnMessage: MIDIData = [
-               144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
-               midiNote, // MIDI Note
-               100, // MIDI Velocity
-             ];
+          const midiPort = 0;
+          const noteDurationMs = 250; // TODO: BETTER
 
-             let noteOffMessage: MIDIData = [
-               128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
-               midiNote, // MIDI Note
-               0, // MIDI Velocity
-             ];
-
-             let midiPort = 0;
-             let noteDurationMs = 250; // TODO: BETTER
-
-             // When scheduling an event to occur in the future, use the current audio context time
-             // multiplied by 1000 (converting seconds to milliseconds) for now.
-             let noteOnEvent = new MIDIEvent(
-               time * 1000,
-               midiPort,
-               noteOnMessage
-             );
-             let noteOffEvent = new MIDIEvent(
-               time * 1000 + noteDurationMs,
-               midiPort,
-               noteOffMessage
-             );
-             padInstance.scheduleEvent(noteOnEvent);
-             padInstance.scheduleEvent(noteOffEvent);
-           }
-         );
-       }
+          // When scheduling an event to occur in the future, use the current audio context time
+          // multiplied by 1000 (converting seconds to milliseconds) for now.
+          const noteOnEvent = new MIDIEvent(
+            time * 1000,
+            midiPort,
+            noteOnMessage,
+          );
+          const noteOffEvent = new MIDIEvent(
+            time * 1000 + noteDurationMs,
+            midiPort,
+            noteOffMessage,
+          );
+          padInstance.scheduleEvent(noteOnEvent);
+          padInstance.scheduleEvent(noteOffEvent);
+        });
+      }
     } else {
       console.log("AudioContext is undefined");
-
+    }
   }
-}
 
   function scheduler() {
     // while there are notes that will need to play before the next interval,
@@ -179,16 +182,16 @@ export default function Button({
         nextNoteTime.current <
         audioContext.current.currentTime + scheduleAheadTime
       ) {
-        scheduleNote(currentStep.current, nextNoteTime.current); //schedule note to play
-        nextNote(); //push to next 16th
+        scheduleNote(currentStep.current, nextNoteTime.current); // schedule note to play
+        nextNote(); // push to next 16th
       }
       timerID.current = window.setTimeout(scheduler, lookahead);
     }
   }
 
-  function handleClick(event:React.MouseEvent<HTMLButtonElement>) {
-    event.stopPropagation() //stop event from firing twice
-    //animation
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation(); // stop event from firing twice
+    // animation
     const anim = actions[names[0]];
     if (anim) {
       anim.reset();
@@ -196,7 +199,7 @@ export default function Button({
       anim.setDuration(0.2);
       anim.play();
     }
-    //logic
+    // logic
     setIsPlaying(!isPlaying);
     if (audioContext.current?.state == "suspended") {
       audioContext.current?.resume();
@@ -207,11 +210,9 @@ export default function Button({
       currentBar.current = 0;
       nextNoteTime.current = audioContext.current.currentTime;
       scheduler(); // kick off scheduling
-      return;
-    } else if (isPlaying){
+    } else if (isPlaying) {
       window.clearTimeout(timerID.current);
-      //console.log("this is the timer ID:", timerID.current)
-      return;
+      // console.log("this is the timer ID:", timerID.current)
     }
   }
 
@@ -222,7 +223,6 @@ export default function Button({
   );
 }
 
-//randomly set button color for each day?
+// randomly set button color for each day?
 
-
-//better trash disposal needed
+// better trash disposal needed
