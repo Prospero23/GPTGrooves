e1dd6cb13b8a70d9d44a3434ab561c1cff828851
Ben Eidson <b.e.eidson@gmail.com>
2023-12-02 20:57:05 -0500

    init audio is the darn problem

--- app/components/assets/Button.tsx
+++ app/components/assets/Button.tsx
@@ -1,150 +1,54 @@
-// /* eslint-disable react/no-unknown-property */
-// import {
-//   useRef,
-//   useState,
-//   useEffect,
-//   type Dispatch,
-//   type SetStateAction,
-// } from "react";
-
-// import { type Vector3 } from "three";
-// import { type ThreeEvent } from "@react-three/fiber";
-// import { useAnimations, useGLTF } from "@react-three/drei";
-// import { A11y, useA11y } from "@react-three/a11y";
-
-// interface ButtonProps {
-//   position: Vector3;
-//   isPlaying: boolean;
-//   setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
-// }
-// function ButtonObject({
-//   position,
-//   setPlayAnimation,
-//   playAnimation,
-// }: {
-//   position: Vector3;
-//   handleClick: (event: ThreeEvent<MouseEvent>) => void;
-//   setPlayAnimation: Dispatch<SetStateAction<boolean>>;
-//   playAnimation: boolean;
-// }) {
-//   const a11y = useA11y();
-//   const group = useRef<THREE.Group | null>(null);
-
-//   // @ts-expect-error nodes are present on GLTF
-//   const { nodes, materials, animations } = useGLTF("/assets/butttton.glb");
-//   const { actions, names } = useAnimations(animations, group);
-
-//   useEffect(() => {
-//     if (playAnimation) {
-//       const anim = actions[names[0]];
-//       if (anim != null) {
-//         anim.reset();
-//         anim.repetitions = 1;
-//         anim.setDuration(0.2);
-//         anim.play();
-//       }
-//       setPlayAnimation(false); // Reset the state
-//     }
-//   }, [playAnimation, actions, names, setPlayAnimation]);
-
-//   return (
-//     <group ref={group} dispose={null} position={position}>
-//       <group name="Scene">
-//         <mesh
-//           name="Cylinder"
-//           castShadow
-//           receiveShadow
-//           geometry={nodes.Cylinder.geometry}
-//           material={materials["Material.001"]}
-//           position={[0.197, 0.055, 0.005]}
-//           scale={[1, 0.056, 1]}
-//         />
-//         <mesh
-//           name="Cylinder001"
-//           castShadow
-//           receiveShadow
-//           geometry={nodes.Cylinder001.geometry}
-//           position={[0.197, 0.147, 0.019]}
-//           rotation={[-Math.PI, 0, -Math.PI]}
-//           scale={[-0.887, -0.122, -0.887]}
-//         >
-//           <meshBasicMaterial
-//             color={a11y.focus || a11y.hover ? "red" : "blue"}
-//           />
-//         </mesh>
-//       </group>
-//     </group>
-//   );
-// }
-
-// export default function Button({
-//   position,
-//   isPlaying,
-//   setIsPlaying,
-// }: ButtonProps) {
-//   const [playAnimation, setPlayAnimation] = useState(false);
-
-//   function handleClick() {
-//     setIsPlaying(!isPlaying);
-//     setPlayAnimation(true); // Add this line to trigger animation
-//   }
-
-//   return (
-//     <A11y role="button" actionCall={handleClick} description="Plays songs">
-//       <ButtonObject
-//         position={position}
-//         handleClick={handleClick}
-//         playAnimation={playAnimation}
-//         setPlayAnimation={setPlayAnimation}
-//       />
-//     </A11y>
-//   );
-// }
-// // randomly set button color for each day?
-// // switch material to look more glass like
-// // need accessisisiblity yo be better
-
 /* eslint-disable react/no-unknown-property */
-import { useRef } from "react";
+import {
+  useRef,
+  useState,
+  useEffect,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 
 import { type Vector3 } from "three";
 import { type ThreeEvent } from "@react-three/fiber";
 import { useAnimations, useGLTF } from "@react-three/drei";
+import { A11y, useA11y } from "@react-three/a11y";
 
 interface ButtonProps {
   position: Vector3;
   isPlaying: boolean;
   setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
-export default function Button({
+function ButtonObject({
   position,
-  isPlaying,
-  setIsPlaying,
-}: ButtonProps) {
+  setPlayAnimation,
+  playAnimation,
+}: {
+  position: Vector3;
+  handleClick: (event: ThreeEvent<MouseEvent>) => void;
+  setPlayAnimation: Dispatch<SetStateAction<boolean>>;
+  playAnimation: boolean;
+}) {
+  const a11y = useA11y();
   const group = useRef<THREE.Group | null>(null);
 
   // @ts-expect-error nodes are present on GLTF
   const { nodes, materials, animations } = useGLTF("/assets/butttton.glb");
   const { actions, names } = useAnimations(animations, group);
 
-  function handleClick(event: ThreeEvent<MouseEvent>) {
-    event.stopPropagation(); // stop event from firing twice
-    // animation
-    const anim = actions[names[0]];
-    if (anim != null) {
-      anim.reset();
-      anim.repetitions = 1;
-      anim.setDuration(0.2);
-      anim.play();
+  useEffect(() => {
+    if (playAnimation) {
+      const anim = actions[names[0]];
+      if (anim != null) {
+        anim.reset();
+        anim.repetitions = 1;
+        anim.setDuration(0.2);
+        anim.play();
+      }
+      setPlayAnimation(false); // Reset the state
     }
-    // logic
-    setIsPlaying(!isPlaying);
-  }
+  }, [playAnimation, actions, names, setPlayAnimation]);
 
   return (
-    // <A11y role="button" description="Triggers audio">
-    <group ref={group} dispose={null} position={position} onClick={handleClick}>
+    <group ref={group} dispose={null} position={position}>
       <group name="Scene">
         <mesh
           name="Cylinder"
@@ -164,10 +68,106 @@ export default function Button({
           rotation={[-Math.PI, 0, -Math.PI]}
           scale={[-0.887, -0.122, -0.887]}
         >
-          <meshBasicMaterial color={"red"} />
+          <meshBasicMaterial
+            color={a11y.focus || a11y.hover ? "red" : "blue"}
+          />
         </mesh>
       </group>
     </group>
-    // </A11y>
   );
 }
+
+export default function Button({
+  position,
+  isPlaying,
+  setIsPlaying,
+}: ButtonProps) {
+  const [playAnimation, setPlayAnimation] = useState(false);
+
+  function handleClick() {
+    setIsPlaying(!isPlaying);
+    setPlayAnimation(true); // Add this line to trigger animation
+  }
+
+  return (
+    <A11y role="button" actionCall={handleClick} description="Plays songs">
+      <ButtonObject
+        position={position}
+        handleClick={handleClick}
+        playAnimation={playAnimation}
+        setPlayAnimation={setPlayAnimation}
+      />
+    </A11y>
+  );
+}
+// randomly set button color for each day?
+// switch material to look more glass like
+// need accessisisiblity yo be better
+
+// /* eslint-disable react/no-unknown-property */
+// import { useRef } from "react";
+
+// import { type Vector3 } from "three";
+// import { type ThreeEvent } from "@react-three/fiber";
+// import { useAnimations, useGLTF } from "@react-three/drei";
+
+// interface ButtonProps {
+//   position: Vector3;
+//   isPlaying: boolean;
+//   setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+// }
+
+// export default function Button({
+//   position,
+//   isPlaying,
+//   setIsPlaying,
+// }: ButtonProps) {
+//   const group = useRef<THREE.Group | null>(null);
+
+//   // @ts-expect-error nodes are present on GLTF
+//   const { nodes, materials, animations } = useGLTF("/assets/butttton.glb");
+//   const { actions, names } = useAnimations(animations, group);
+
+//   function handleClick(event: ThreeEvent<MouseEvent>) {
+//     event.stopPropagation(); // stop event from firing twice
+//     // animation
+//     const anim = actions[names[0]];
+//     if (anim != null) {
+//       anim.reset();
+//       anim.repetitions = 1;
+//       anim.setDuration(0.2);
+//       anim.play();
+//     }
+//     // logic
+//     setIsPlaying(!isPlaying);
+//   }
+
+//   return (
+//     // <A11y role="button" description="Triggers audio">
+//     <group ref={group} dispose={null} position={position} onClick={handleClick}>
+//       <group name="Scene">
+//         <mesh
+//           name="Cylinder"
+//           castShadow
+//           receiveShadow
+//           geometry={nodes.Cylinder.geometry}
+//           material={materials["Material.001"]}
+//           position={[0.197, 0.055, 0.005]}
+//           scale={[1, 0.056, 1]}
+//         />
+//         <mesh
+//           name="Cylinder001"
+//           castShadow
+//           receiveShadow
+//           geometry={nodes.Cylinder001.geometry}
+//           position={[0.197, 0.147, 0.019]}
+//           rotation={[-Math.PI, 0, -Math.PI]}
+//           scale={[-0.887, -0.122, -0.887]}
+//         >
+//           <meshBasicMaterial color={"red"} />
+//         </mesh>
+//       </group>
+//     </group>
+//     // </A11y>
+//   );
+// }
--- app/components/assets/InitAudio.tsx
+++ app/components/assets/InitAudio.tsx
@@ -1,5 +1,72 @@
-import { A11y, useA11y } from "@react-three/a11y";
+// import { A11y, useA11y } from "@react-three/a11y";
+// import { Text3D, Center } from "@react-three/drei"; // Changed from Text3D to Text as Text3D is not a standard export from '@react-three/drei'
+
+// interface InitAudioProps {
+//   isInitialized: boolean;
+//   set: React.Dispatch<React.SetStateAction<boolean>>; // Updated the type definitions
+//   initializeAudio: () => Promise<void>;
+// }
+
+// function InitAudioText({
+//   isShowing,
+//   handleClick,
+// }: {
+//   isShowing: boolean;
+//   handleClick: () => Promise<void>;
+// }) {
+//   const a11y = useA11y();
+//   return (
+//     <Center top>
+//       <Text3D
+//         font={"/fonts/ChicagoFLF_Regular.json"}
+//         scale={2.5}
+//         position={isShowing ? [-10, 0, 0] : [-10, -50, 0]}
+//         castShadow
+//         onClick={handleClick}
+//       >
+//         <meshLambertMaterial
+//           color={a11y.hover || a11y.focus ? "red" : "gray"}
+//           emissive={a11y.hover || a11y.focus ? "red" : "black"}
+//         />
+//         Initialize Audio
+//       </Text3D>
+//     </Center>
+//   );
+// }
+
+// const InitAudio: React.FC<InitAudioProps> = ({
+//   isInitialized,
+//   initializeAudio,
+//   set,
+// }: InitAudioProps) => {
+//   const isShowing = !isInitialized;
+
+//   async function handleClick() {
+//     await initializeAudio();
+//     set(!isInitialized);
+//   }
+
+//   return (
+//     <>
+//       {isShowing && ( // render the Text3D only if isInitialized is false
+//         <A11y
+//           role="button"
+//           actionCall={async () => {
+//             await handleClick();
+//           }}
+//           description="Text to initialize audio processes"
+//         >
+//           <InitAudioText handleClick={handleClick} isShowing={isShowing} />
+//         </A11y>
+//       )}
+//     </>
+//   );
+// };
+
+// export default InitAudio;
+
 import { Text3D, Center } from "@react-three/drei"; // Changed from Text3D to Text as Text3D is not a standard export from '@react-three/drei'
+import { useEffect, useState } from "react"; // Removed 'type' imports, as they're not standard in JavaScript or TypeScript
 
 interface InitAudioProps {
   isInitialized: boolean;
@@ -7,57 +74,61 @@ interface InitAudioProps {
   initializeAudio: () => Promise<void>;
 }
 
-function InitAudioText({
-  isShowing,
-  handleClick,
-}: {
-  isShowing: boolean;
-  handleClick: () => Promise<void>;
-}) {
-  const a11y = useA11y();
-  return (
-    <Center top>
-      <Text3D
-        font={"/fonts/ChicagoFLF_Regular.json"}
-        scale={2.5}
-        position={isShowing ? [-10, 0, 0] : [-10, -50, 0]}
-        castShadow
-        onClick={handleClick}
-      >
-        <meshLambertMaterial
-          color={a11y.hover || a11y.focus ? "red" : "gray"}
-          emissive={a11y.hover || a11y.focus ? "red" : "black"}
-        />
-        Initialize Audio
-      </Text3D>
-    </Center>
-  );
-}
-
 const InitAudio: React.FC<InitAudioProps> = ({
   isInitialized,
   initializeAudio,
   set,
 }: InitAudioProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const textColor = isHovered ? "red" : "black";
+  const textEmmisive = isHovered ? "red" : "gray";
   const isShowing = !isInitialized;
+
+  function handleEnter() {
+    setIsHovered(true);
+  }
+
+  function handleLeave() {
+    setIsHovered(false);
+  }
 
   async function handleClick() {
     await initializeAudio();
+    console.log("smh");
     set(!isInitialized);
   }
+
+  // change cursor whenever hovered
+  useEffect(() => {
+    if (isHovered) {
+      document.body.style.cursor = "pointer";
+    } else {
+      document.body.style.cursor = "default";
+    }
+
+    // Cleanup function to reset cursor when component is unmounted
+    return () => {
+      document.body.style.cursor = "default";
+    };
+  }, [isHovered]);
 
   return (
     <>
       {isShowing && ( // render the Text3D only if isInitialized is false
-        <A11y
-          role="button"
-          actionCall={async () => {
-            await handleClick();
-          }}
-          description="Text to initialize audio processes"
-        >
-          <InitAudioText handleClick={handleClick} isShowing={isShowing} />
-        </A11y>
+        <Center top>
+          <Text3D
+            font={"/fonts/ChicagoFLF_Regular.json"}
+            scale={2.5}
+            position={isShowing ? [-10, 0, 0] : [-10, -50, 0]}
+            castShadow
+            onPointerEnter={handleEnter}
+            onPointerLeave={handleLeave}
+            onClick={handleClick}
+          >
+            <meshLambertMaterial color={textColor} emissive={textEmmisive} />
+            Initialize Audio
+          </Text3D>
+        </Center>
       )}
     </>
   );
