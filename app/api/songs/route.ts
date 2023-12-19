7dda926212586d31ebf5bea7c8514bd912753022
Ben Eidson <b.e.eidson@gmail.com>
2023-12-19 17:13:46 -0500

    added start of api route to fix caching errors attempt again

--- app/api/songs/route.ts
+++ app/api/songs/route.ts
@@ -3,7 +3,9 @@ import { getSongs } from "@/library/db";
 export async function GET(request: Request) {
   try {
     const songs = await getSongs();
-    return Response.json({ songs });
+    return new Response(JSON.stringify({ songs }), {
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ error: "Error fetching songs" }), {
