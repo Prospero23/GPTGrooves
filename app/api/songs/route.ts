6fe79ccb64f3d61e217a3cb8f2a3e0972c7b6e42
Ben Eidson <b.e.eidson@gmail.com>
2023-12-20 12:40:19 -0500

    switched to using fetch

--- app/api/songs/route.ts
+++ app/api/songs/route.ts
@@ -1,11 +1,10 @@
 import { getSongs } from "@/library/db";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
     const songs = await getSongs();
-    return new Response(JSON.stringify({ songs }), {
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json({ songs });
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ error: "Error fetching songs" }), {
@@ -14,3 +13,8 @@ export async function GET(request: Request) {
     });
   }
 }
+
+// export async function GET(request: Request) {
+//   const songs = await getSongs();
+//   return NextResponse.json({ songs });
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import { getSongs } from "@/library/db";
+import { type SongType } from "@/library/musicData";
 import Scene from "./components/Scene";
 
 // import PlayButton from "./components/PlayButton";
 export default async function Home() {
   let result;
   try {
-    result = await getSongs();
+    result = await fetch(`${process.env.BASE_URL}/api/songs`);
   } catch (e) {
     throw Error("Error fetching songs");
   }
-  const bars = result;
+  const body = await result.json();
+  const bars: SongType[] = body.songs;
+  console.log(bars);
+
   const songsArray = bars.map((item) => item.song);
   const datesArray = bars.map((item) => {
     const utcDate = new Date(item.created_at_utc);
@@ -21,6 +24,7 @@ export default async function Home() {
   return (
     <main className="flex w-screen h-screen bg-white">
       <div className="w-screen h-screen">
+        <p>{"hi"}</p>
         <Scene songs={songsArray} dates={datesArray} />
       </div>
     </main>
